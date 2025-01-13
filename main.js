e1f065b1553f7a5751a8e539c138239aa415a0dd
Supragy352 <mishra03supragya@gmail.com>
2025-01-13 17:20:05 +0530

    Edited path using config.js

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   base: './',
+// });
+
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
-const loader = new GLTFLoader();
 
-// loader.load( './static/sugar_candy.gltf', function ( gltf ) {
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-// 	scene.add( gltf.scene );
-
-// }, undefined, function ( error ) {
-
-// 	console.error( error );
-
-// } );
-
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 camera.position.z = 5;
 
 function animate() {
-	renderer.render( scene, camera );
-	cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-}
 
-renderer.setAnimationLoop( animate );
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+
+}
